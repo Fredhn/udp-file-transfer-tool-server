e9be37cfe879d875fe664b97566d517e00414fc9
Fred Neves <fred_div@hotmail.com>
2017-04-27 23:58:11 -0300

    Add files via upload

---
+++ FileTransfer_Server.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FileTransfer_Server
+{
+    partial class FileTransfer_Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FileTransfer_Server));
+            this.pnl_Main = new System.Windows.Forms.Panel();
+            this.pnl_Main2 = new System.Windows.Forms.Panel();
+            this.lbl_IP = new System.Windows.Forms.Label();
+            this.lbl_localhostIPTitle = new System.Windows.Forms.Label();
+            this.pb_ipConnection = new System.Windows.Forms.PictureBox();
+            this.pnl_MainSeparador = new System.Windows.Forms.Panel();
+            this.pb_ShutdownButton = new System.Windows.Forms.PictureBox();
+            this.listbox_Connections = new System.Windows.Forms.ListBox();
+            this.lbl_MainTitle = new System.Windows.Forms.Label();
+            this.pnl_Main.SuspendLayout();
+            this.pnl_Main2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ipConnection)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ShutdownButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Main
+            // 
+            this.pnl_Main.BackColor = System.Drawing.Color.Firebrick;
+            this.pnl_Main.Controls.Add(this.pnl_Main2);
+            this.pnl_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Main.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Main.Name = "pnl_Main";
+            this.pnl_Main.Size = new System.Drawing.Size(284, 262);
+            this.pnl_Main.TabIndex = 0;
+            // 
+            // pnl_Main2
+            // 
+            this.pnl_Main2.BackColor = System.Drawing.Color.SeaGreen;
+            this.pnl_Main2.Controls.Add(this.lbl_IP);
+            this.pnl_Main2.Controls.Add(this.lbl_localhostIPTitle);
+            this.pnl_Main2.Controls.Add(this.pb_ipConnection);
+            this.pnl_Main2.Controls.Add(this.pnl_MainSeparador);
+            this.pnl_Main2.Controls.Add(this.pb_ShutdownButton);
+            this.pnl_Main2.Controls.Add(this.listbox_Connections);
+            this.pnl_Main2.Controls.Add(this.lbl_MainTitle);
+            this.pnl_Main2.Location = new System.Drawing.Point(5, 5);
+            this.pnl_Main2.Name = "pnl_Main2";
+            this.pnl_Main2.Size = new System.Drawing.Size(274, 252);
+            this.pnl_Main2.TabIndex = 0;
+            // 
+            // lbl_IP
+            // 
+            this.lbl_IP.AutoSize = true;
+            this.lbl_IP.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_IP.ForeColor = System.Drawing.Color.Transparent;
+            this.lbl_IP.Location = new System.Drawing.Point(84, 227);
+            this.lbl_IP.Name = "lbl_IP";
+            this.lbl_IP.Size = new System.Drawing.Size(0, 18);
+            this.lbl_IP.TabIndex = 10;
+            // 
+            // lbl_localhostIPTitle
+            // 
+            this.lbl_localhostIPTitle.AutoSize = true;
+            this.lbl_localhostIPTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_localhostIPTitle.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_localhostIPTitle.ForeColor = System.Drawing.Color.Transparent;
+            this.lbl_localhostIPTitle.Location = new System.Drawing.Point(84, 204);
+            this.lbl_localhostIPTitle.Name = "lbl_localhostIPTitle";
+            this.lbl_localhostIPTitle.Size = new System.Drawing.Size(107, 20);
+            this.lbl_localhostIPTitle.TabIndex = 9;
+            this.lbl_localhostIPTitle.Text = "Localhost IP:";
+            // 
+            // pb_ipConnection
+            // 
+            this.pb_ipConnection.Image = global::FileTransfer_Server.Properties.Resources.Network_Internet_Connection_icon;
+            this.pb_ipConnection.Location = new System.Drawing.Point(26, 201);
+            this.pb_ipConnection.Name = "pb_ipConnection";
+            this.pb_ipConnection.Size = new System.Drawing.Size(48, 48);
+            this.pb_ipConnection.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pb_ipConnection.TabIndex = 8;
+            this.pb_ipConnection.TabStop = false;
+            // 
+            // pnl_MainSeparador
+            // 
+            this.pnl_MainSeparador.BackColor = System.Drawing.Color.Firebrick;
+            this.pnl_MainSeparador.Location = new System.Drawing.Point(-4, 196);
+            this.pnl_MainSeparador.Name = "pnl_MainSeparador";
+            this.pnl_MainSeparador.Size = new System.Drawing.Size(280, 5);
+            this.pnl_MainSeparador.TabIndex = 7;
+            // 
+            // pb_ShutdownButton
+            // 
+            this.pb_ShutdownButton.Image = global::FileTransfer_Server.Properties.Resources.power_2_icon__1_;
+            this.pb_ShutdownButton.Location = new System.Drawing.Point(247, 3);
+            this.pb_ShutdownButton.Name = "pb_ShutdownButton";
+            this.pb_ShutdownButton.Size = new System.Drawing.Size(24, 24);
+            this.pb_ShutdownButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pb_ShutdownButton.TabIndex = 4;
+            this.pb_ShutdownButton.TabStop = false;
+            this.pb_ShutdownButton.Click += new System.EventHandler(this.pb_ShutdownButton_Click);
+            // 
+            // listbox_Connections
+            // 
+            this.listbox_Connections.FormattingEnabled = true;
+            this.listbox_Connections.Location = new System.Drawing.Point(43, 49);
+            this.listbox_Connections.Name = "listbox_Connections";
+            this.listbox_Connections.Size = new System.Drawing.Size(188, 134);
+            this.listbox_Connections.TabIndex = 2;
+            // 
+            // lbl_MainTitle
+            // 
+            this.lbl_MainTitle.AutoSize = true;
+            this.lbl_MainTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl_MainTitle.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MainTitle.ForeColor = System.Drawing.Color.Transparent;
+            this.lbl_MainTitle.Location = new System.Drawing.Point(43, 14);
+            this.lbl_MainTitle.Name = "lbl_MainTitle";
+            this.lbl_MainTitle.Size = new System.Drawing.Size(188, 25);
+            this.lbl_MainTitle.TabIndex = 1;
+            this.lbl_MainTitle.Text = "File Transfer Tool";
+            // 
+            // FileTransfer_Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pnl_Main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FileTransfer_Server";
+            this.Text = "File Transfer Tool - Server";
+            this.Load += new System.EventHandler(this.FileTransfer_Server_Load);
+            this.pnl_Main.ResumeLayout(false);
+            this.pnl_Main2.ResumeLayout(false);
+            this.pnl_Main2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ipConnection)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ShutdownButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Main;
+        private System.Windows.Forms.Panel pnl_Main2;
+        private System.Windows.Forms.ListBox listbox_Connections;
+        private System.Windows.Forms.Label lbl_MainTitle;
+        private System.Windows.Forms.PictureBox pb_ShutdownButton;
+        private System.Windows.Forms.PictureBox pb_ipConnection;
+        private System.Windows.Forms.Panel pnl_MainSeparador;
+        private System.Windows.Forms.Label lbl_IP;
+        private System.Windows.Forms.Label lbl_localhostIPTitle;
+    }
+}
+
---
+++ FileTransfer_Server.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileTransfer_Server
+{
+    public partial class FileTransfer_Server : Form
+    {
+        private ArrayList nSockets;
+
+        public string SaveFileName = string.Empty;
+        private const int BufferSize = 1024;
+        public string Status = string.Empty;
+        public Thread T = null;
+        private object sync_temp = new object();
+
+        public FileTransfer_Server()
+        {
+            InitializeComponent();
+        }
+
+        private void pb_ShutdownButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //private void FileTransfer_Server_Load(object sender, EventArgs e)
+        //{
+        //    IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+        //    lbl_IP.Text = IPHost.AddressList[0].ToString();
+        //    nSockets = new ArrayList();
+        //    Thread thdListener = new Thread(new ThreadStart(listenerThread));
+        //    thdListener.Start();
+        //}
+
+        //public void listenerThread()
+        //{
+
+        //    TcpListener tcpListener = new TcpListener(8080);
+        //    tcpListener.Start();
+        //    while (true)
+        //    {
+        //        Socket handlerSocket = tcpListener.AcceptSocket();
+        //        if (handlerSocket.Connected)
+        //        {
+        //            Control.CheckForIllegalCrossThreadCalls = false;
+        //            listbox_Connections.Items.Add(handlerSocket.RemoteEndPoint.ToString() + " connected.");
+        //            lock (this)
+        //            {
+        //                nSockets.Add(handlerSocket);
+        //            }
+        //            ThreadStart thdstHandler = new ThreadStart(handlerThread);
+        //            Thread thdHandler = new Thread(thdstHandler);
+        //            thdHandler.Start();
+        //        }
+        //    }
+        //}
+        //public void handlerThread()
+        //{
+        //    Socket handlerSocket = (Socket)nSockets[nSockets.Count - 1];
+        //    NetworkStream networkStream = new NetworkStream(handlerSocket);
+        //    int thisRead = 0;
+        //    int blockSize = 1024;
+        //    Byte[] dataByte = new Byte[blockSize];
+        //    try
+        //    { 
+        //        lock (this)
+        //        {
+        //            // Only one process can access
+        //            // the same file at any given time
+        //            Stream fileStream = File.OpenWrite("C:\\Users\\FRED\\Gabi\\Documents\\SubmittedFile.pdf");
+        //            while (true)
+        //            {
+        //                thisRead = networkStream.Read(dataByte, 0, blockSize);
+        //                fileStream.Write(dataByte, 0, thisRead);
+        //                if (thisRead == 0) break;
+        //            }
+        //            fileStream.Close();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        //netstream.Close();
+        //    }
+
+        //    listbox_Connections.Items.Add("Arquivo salvo.");
+        //    handlerSocket = null;
+        //}
+
+        private void FileTransfer_Server_Load(object sender, EventArgs e)
+        {
+            IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+            lbl_IP.Text = IPHost.AddressList[0].ToString();
+            nSockets = new ArrayList();
+
+            ThreadStart Ts = new ThreadStart(StartReceiving);
+            T = new Thread(Ts);
+            T.Start();
+        }
+
+        public void StartReceiving()
+        {
+            ReceiveTCP(8080);
+        }
+
+
+        public void ReceiveTCP(int portN)
+        {
+            TcpListener Listener = null;
+            try
+            {
+                Listener = new TcpListener(IPAddress.Any, portN);
+                Listener.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            byte[] RecData = new byte[BufferSize];
+            int RecBytes;
+
+            for (;;)
+            {
+                TcpClient client = null;
+                NetworkStream netstream = null;
+                Status = string.Empty;
+                try
+                {
+                    string message = "Aceitar arquivo recebido? ";
+                    string caption = "Conexão solicitada";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result;
+
+                    if (Listener.Pending())
+                    {
+                        client = Listener.AcceptTcpClient();
+                        netstream = client.GetStream();
+                        //Status = "Connected to a client\n";
+                        //Task.Factory.StartNew(() =>
+                        //{
+                        //    this.BeginInvoke(new Action(() =>
+                        //    {
+                        //        listbox_Connections.Items.Add(Status);
+                        //    }));
+                        //});
+                        //listbox_Connections.Items.Add(Status);
+                        result = MessageBox.Show(message, caption, buttons);
+
+                        if (result == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            SaveFileDialog DialogSave = new SaveFileDialog();
+                            DialogSave.Filter = "pdf files (*.pdf)|*.pdf|mp3 files (*.mp3)|*.mp3";
+                            DialogSave.RestoreDirectory = true;
+                            DialogSave.Title = "Onde deseja salvar o arquivo?";
+                            DialogSave.InitialDirectory = @"C:/";
+
+                            //if (DialogSave.ShowDialog() == DialogResult.OK)
+                            //    SaveFileName = DialogSave.FileName;
+
+                            Action ac = () => { lock (sync_temp) { DialogSave.ShowDialog(); } };
+                            Invoke(ac);
+
+                            lock (sync_temp)
+                            {
+                                SaveFileName = DialogSave.FileName;
+                            }
+
+                            if (SaveFileName != string.Empty)
+                            {
+                                int totalrecbytes = 0;
+                                FileStream Fs = new FileStream(SaveFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                                while ((RecBytes = netstream.Read(RecData, 0, RecData.Length)) > 0)
+                                {
+                                    Fs.Write(RecData, 0, RecBytes);
+                                    totalrecbytes += RecBytes;
+                                }
+                                Fs.Close();
+                                //listbox_Connections.Items.Add("Recebido: " + SaveFileName.ToString());
+                            }
+                           
+                            netstream.Close();
+                            client.Close();
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //netstream.Close();
+                }
+            }
+        }
+    }
+}
