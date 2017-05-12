364560c59519fef1127e7e46137271f599b57db6
Fred Neves <fred_div@hotmail.com>
2017-05-12 02:05:07 -0300

    Update FileTransfer_Server.cs

--- FileTransfer_Server.cs
+++ FileTransfer_Server.cs
@@ -35,70 +35,6 @@ namespace FileTransfer_Server
             Application.Exit();
         }
 
-        //private void FileTransfer_Server_Load(object sender, EventArgs e)
-        //{
-        //    IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
-        //    lbl_IP.Text = IPHost.AddressList[0].ToString();
-        //    nSockets = new ArrayList();
-        //    Thread thdListener = new Thread(new ThreadStart(listenerThread));
-        //    thdListener.Start();
-        //}
-
-        //public void listenerThread()
-        //{
-
-        //    TcpListener tcpListener = new TcpListener(8080);
-        //    tcpListener.Start();
-        //    while (true)
-        //    {
-        //        Socket handlerSocket = tcpListener.AcceptSocket();
-        //        if (handlerSocket.Connected)
-        //        {
-        //            Control.CheckForIllegalCrossThreadCalls = false;
-        //            listbox_Connections.Items.Add(handlerSocket.RemoteEndPoint.ToString() + " connected.");
-        //            lock (this)
-        //            {
-        //                nSockets.Add(handlerSocket);
-        //            }
-        //            ThreadStart thdstHandler = new ThreadStart(handlerThread);
-        //            Thread thdHandler = new Thread(thdstHandler);
-        //            thdHandler.Start();
-        //        }
-        //    }
-        //}
-        //public void handlerThread()
-        //{
-        //    Socket handlerSocket = (Socket)nSockets[nSockets.Count - 1];
-        //    NetworkStream networkStream = new NetworkStream(handlerSocket);
-        //    int thisRead = 0;
-        //    int blockSize = 1024;
-        //    Byte[] dataByte = new Byte[blockSize];
-        //    try
-        //    { 
-        //        lock (this)
-        //        {
-        //            // Only one process can access
-        //            // the same file at any given time
-        //            Stream fileStream = File.OpenWrite("C:\\Users\\FRED\\Gabi\\Documents\\SubmittedFile.pdf");
-        //            while (true)
-        //            {
-        //                thisRead = networkStream.Read(dataByte, 0, blockSize);
-        //                fileStream.Write(dataByte, 0, thisRead);
-        //                if (thisRead == 0) break;
-        //            }
-        //            fileStream.Close();
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //        //netstream.Close();
-        //    }
-
-        //    listbox_Connections.Items.Add("Arquivo salvo.");
-        //    handlerSocket = null;
-        //}
-
         private void FileTransfer_Server_Load(object sender, EventArgs e)
         {
             IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
@@ -148,15 +84,6 @@ namespace FileTransfer_Server
                     {
                         client = Listener.AcceptTcpClient();
                         netstream = client.GetStream();
-                        //Status = "Connected to a client\n";
-                        //Task.Factory.StartNew(() =>
-                        //{
-                        //    this.BeginInvoke(new Action(() =>
-                        //    {
-                        //        listbox_Connections.Items.Add(Status);
-                        //    }));
-                        //});
-                        //listbox_Connections.Items.Add(Status);
                         result = MessageBox.Show(message, caption, buttons);
 
                         if (result == System.Windows.Forms.DialogResult.Yes)
@@ -200,7 +127,6 @@ namespace FileTransfer_Server
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
-                    //netstream.Close();
                 }
             }
         }
